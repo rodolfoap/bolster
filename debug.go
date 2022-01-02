a51df7a3a03f4a7f4b48d0adde5c57ff8e5a2893
RodolfoAP <rodolfoap@gmail.com>
2022-01-02 11:02:29 +0100

    Changes 2022/01/02-11:02:29

--- debug.go
+++ debug.go
@@ -4,7 +4,7 @@ import("log"; "fmt"; "runtime";)
 func Println(msgs ...interface{}) {
 	pc:=make([]uintptr, 1)
 	frame, _:=runtime.CallersFrames(pc[:runtime.Callers(2, pc)]).Next()
-	fmt.Println(frame.File, frame.Function, "["+frame.Line+"]:", msgs...)
+	fmt.Println(frame.File, frame.Function, "["+string(frame.Line)+"]:", msgs...)
 }
 func Printf(format string, msgs ...interface{}) {
 	pc:=make([]uintptr, 1)
