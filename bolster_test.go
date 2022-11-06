147c1fa32abd3ee007c451b7ae453c54710d37f7
RodolfoAP <rodolfo.alcazar@gtd.eu>
2022-11-06 07:51:05 +0100

    Added Log(), Logf(), Tracef()

--- bolster_test.go
+++ bolster_test.go
@@ -49,15 +49,21 @@ func Test_lib(t *testing.T) {
 	Trace(9, 1, 3)
 	// /home/rap/git/gobox/54-bolster.use/app.go main.main() [49] 9 1 3
 	TimeTrace("Using log")
-	// 2022/11/05 17:09:24 /home/rap/git/gobox/54-bolster.use/app.go main.main() [51] Using log
+	// 2022/11/06 07:49:30 /home/rap/git/bolster/bolster_test.go github.com/rodolfoap/bolster.Test_lib() [51] Using log
+	Log("Using simple log")
+	// 2022/11/06 07:45:54 /home/rap/git/bolster/bolster_test.go github.com/rodolfoap/bolster.Test_lib() [53] Using simple log
+	Logf("Formatted log: Type of '1': %T", 1) // Does not need CR/LF
+	// 2022/11/06 07:47:27 /home/rap/git/bolster/bolster_test.go github.com/rodolfoap/bolster.Test_lib() [55] Formatted log: Type of '1': int
+	Tracef("Formatted TRACE: Type of '1': %T", 1) // Does not need CR/LF
+	// /home/rap/git/bolster/bolster_test.go github.com/rodolfoap/bolster.Test_lib() [57] Formatted TRACE: Type of '1': int
 
 	_, err:=os.Open("I.dont.exist")
 	Error(err)
-	// 2022/11/05 17:09:24 /home/rap/git/gobox/54-bolster.use/app.go main.main() [55] ERROR: open I.dont.exist: no such file or directory
+	// 2022/11/05 17:09:24 /home/rap/git/gobox/54-bolster.use/app.go main.main() [61] ERROR: open I.dont.exist: no such file or directory
 
 	_, err=os.Open("I.dont.exist")
 	Fatal(err)
-	// 2022/11/05 17:09:24 /home/rap/git/gobox/54-bolster.use/app.go main.main() [59] FATAL: open I.dont.exist: no such file or directory
+	// 2022/11/05 17:09:24 /home/rap/git/gobox/54-bolster.use/app.go main.main() [65] FATAL: open I.dont.exist: no such file or directory
 
 	// Will not run, the FATAL error has exited.
 	Printhr()
